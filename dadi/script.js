4b4740c5ea79fe3709e91a011a08568b2c4ef80e
AgneseBettio <bettio.agnese@gmail.com>
2021-04-09 17:06:59 +0200

    inizializzazione

---
+++ dadi/script.js
@@ -0,0 +1,9 @@
+// Generare un numero random da 1 a 6, 
+// sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+var playIt = "dado";
+var startTheGame = prompt("scrivi 'dado' per iniziare a giocare")
+if(playIt !== stratTheGame){
+    alert("sei sicuro che hai scitto 'dado'?")
+}
